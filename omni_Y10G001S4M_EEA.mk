bd36c78e939d38de1fbd091829034d390d8fa85b
Cédric Dnst <amazedtak.pro@gmail.com>
2020-06-22 21:16:37 +0200

    Major Changes

--- omni_Y10G001S4M_EEA.mk
+++ omni_Y10G001S4M_EEA.mk
@@ -1,11 +1,7 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-
-# Another common config inclusion
 $(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
 
-# If you are building from OmniROM's minimal source, Inherit some common Omni stuff.
+# Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 
 PRODUCT_COPY_FILES += device/UNOWHY/Y10G001S4M_EEA/prebuilt/zImage:kernel
@@ -18,7 +14,33 @@ PRODUCT_BRAND := UNOWHY
 PRODUCT_MODEL := Y10G001S4M_EEA
 PRODUCT_MANUFACTURER := malata
 
-# Forcefully add mtp support (adb is already there)
+# From full_base.mk
+# Put fr_FR first in the list, so make it default.
+PRODUCT_LOCALES := fr_FR
+
+# From runtime_libart.mk
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    dalvik.vm.image-dex2oat-Xms=64m \
+    dalvik.vm.image-dex2oat-Xmx=64m \
+    dalvik.vm.dex2oat-Xms=64m \
+    dalvik.vm.dex2oat-Xmx=512m \
+    ro.dalvik.vm.native.bridge=0
+
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    pm.dexopt.first-boot=interpret-only \
+    pm.dexopt.boot=verify-profile \
+    pm.dexopt.install=interpret-only \
+    pm.dexopt.bg-dexopt=speed-profile \
+    pm.dexopt.ab-ota=speed-profile \
+    pm.dexopt.nsys-library=speed \
+    pm.dexopt.shared-apk=speed \
+    pm.dexopt.forced-dexopt=speed \
+    pm.dexopt.core-app=speed \
+    dalvik.vm.image-dex2oat-filter=verify-at-runtime \
+    dalvik.vm.dex2oat-filter=verify-at-runtime \
+    dalvik.vm.usejit=true
+
+# adb is already there, so just add mtp for now
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp
 
@@ -26,3 +48,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="UNOWHY/Y10G001S4M_EEA/Y10G001S4M:9/PPR1.180610.011/1564163634:user/release-keys" \
     PRIVATE_BUILD_DESC="full_unowhy_k1002-user 9 PPR1.180610.011 user.xmlyz.1564163634 release-keys"
+
+    # try force addition of fingerprint
+BUILD_FINGERPRINT := "UNOWHY/Y10G001S4M_EEA/Y10G001S4M:9/PPR1.180610.011/1564163634:user/release-keys"
